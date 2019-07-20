e933cc7c5bd8fdb38d7ba5e018449ad4122c255e
Horace Velmont <dragond7@naver.com>
2019-07-20 14:10:13 +0900

    intial setup

---
+++ src/main/java/org/study/Main.java
@@ -0,0 +1,13 @@
+package org.study;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class Main {
+	public static void main(String[] args) {
+		ActorSystem actorSystem = ActorSystem.create("TestSystem");
+		ActorRef ping = actorSystem.actorOf(Props.create(PingActor.class), "pingActor");
+		ping.tell("start", ActorRef.noSender());
+	}
+}
